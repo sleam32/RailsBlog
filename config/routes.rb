aad605b930011f3f46c3d50a439ed2d3b21ced5d
Shearry Malone <sleam32@gmail.com>
2013-06-03 09:32:34 -0500

    Fix Indentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RailsBlog::Application.routes.draw do
+
+  resources :posts, only: :index
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- test/integration/anonymous_posts_article_test.rb
+++ test/integration/anonymous_posts_article_test.rb
@@ -3,14 +3,12 @@ require 'test_helper'
 class AnonymousPostsArticleTest < ActionDispatch::IntegrationTest
   test "view a list of articles" do
     Post.create!(title: "First Post!!", body: "This is awkward... I have nothing to say :(")
-    Post.create!(title: "Wow, it's been a while!", body: "And yet, I'm still not a writter :( :( lolol")
-
-    visit '/'
-    click_link "View All Posts"
-    assert_current_path '/posts'
-    # assume we have a page variable
-    assert_include page.content, "First Post!!"
+        Post.create!(title: "Wow, it's been a while!", body: "And yet, I'm still not a writter :( :( lolol")
+            visit '/posts'
+            assert_equal Post.count
+            # assert page.has_content? "First Post!"
+            assert_include page.body, "First Post!!"
     #=> assert page.content.include? "First Post!!"
-    assert_include page.content, "Wow, it's been a while!"
-  end
+    assert_include page.body, "Wow, it's been a while!"
+end
 end
