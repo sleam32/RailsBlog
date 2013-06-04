dd03ebbd45096e5beee1f9a9ec744cca23a2540b
Shearry Malone <sleam32@gmail.com>
2013-06-04 10:47:11 -0500

    Rename integration test

--- test/integration/anonymous_posts_article_test.rb
+++ test/integration/anonymous_posts_article_test.rb
@@ -1,13 +1,8 @@
 require 'test_helper'
 
 class AnonymousPostsArticleTest < ActionDispatch::IntegrationTest
-  test "view a list of articles" do
-    Post.create!(title: "First Post!!", body: "This is awkward... I have nothing to say :(")
-      Post.create!(title: "Wow, it's been a while!", body: "And yet, I'm still not a writter :( :( lolol")
-        assert_equal 2, Post.count
-        visit '/posts'
-        assert_include page.body, "All Posts!!"
-        assert_include page.body, "First Post!!"
-        assert_include page.body, "Wow, it's been a while!"
-      end
+  test "posting a new article"  do
+
+  end
+
     end
